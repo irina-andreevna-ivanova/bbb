9abcd7792acd57a45b91219e25db735fae8aec6c
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-06-22 18:28:19 +0300

    Initial commit

---
+++ src/a/b/c/Dummy1.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 6/22/2018.
+ * Project: bbb
+ * *******************************
+ */
+public class Dummy1 {
+}
